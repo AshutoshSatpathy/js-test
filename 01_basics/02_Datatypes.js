95758a9b1f47925559fe9050aea4fb48a4997118
AshutoshSatpathy <ashutosh9438142981@gmail.com>
2024-06-30 08:49:13 +0000

    datatype.js

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,29 @@
+"use strict";// treat all java code as a new version
+
+// alert (3+3) // we are using node js, not browser
+
+console.log(3 +
+     3); // code readability should be high
+
+console.log("Ashu")
+
+
+let name = "Ashu"
+let age = 15
+let isLoggedIn = false
+
+/*primitive datatype
+=====================*/
+//number => 2 to power 53
+//Bigint
+//String
+//boolean => standalone value
+//undefined => value is not mentioned
+//symbol => unique
+
+//object 
+
+console.log(typeof undefined)// undefined
+console.log(typeof null)// object
+
+
