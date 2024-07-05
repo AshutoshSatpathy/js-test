d98ee4790aedd09f4eaa6f253ffcbb1d4f0e09c7
AshutoshSatpathy <ashutosh9438142981@gmail.com>
2024-07-05 11:52:35 +0000

    datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+
+//best comparision(same data types)
+//======================
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+//not best comparision(different data types)
+//======================
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+//confusing comparision
+//=======================
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log (undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+// ===
+console.log("2" === 2)
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,30 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // undefined
+
+// const id = symbol('123')
+//  const anotherId = symbol('123')
+
+// console.log(id === anotherId);
+
+//const bigNumber = 242535664635253n
+
+//Referance (Non primitive)
+
+// Array, objects, Functions
+
+// const heros = ["shaktiman", "naagraj", "Doga"]
+// //objects 
+// let myObj = {
+//     name: "Ashu",
+//     age: 25,
+// }
+
+// const myFunction = function(){
+//   console.log("Hello World")
+// }
+
+// console.log (typeof bigNumber);
